e785153a97afceb187c97c22a63468b20f7ad590
talentamaulana10 <tatamf.id@gmail.com>
2019-03-26 13:51:08 +0700

    make a comparison between the two function methods

--- src/react/es6 refresser/arrowFunction/ArF.js
+++ src/react/es6 refresser/arrowFunction/ArF.js
@@ -2,8 +2,14 @@ import React, { Component } from 'react'
 
 export default class ArF extends Component {
   render() {
-      const myFunction = number => number*number;
-      console.log(myFunction(5))
+      const jobs = [
+          { id:1, isActive:true },
+          { id:2, isActive:true },
+          { id:3, isActive:false }
+      ]
+      const ordinaryFunction = jobs.filter(function(job) {return job.isActive} ) 
+      const simpleFunction = jobs.filter(job=>job.isActive) 
+      console.log(simpleFunction)
     return (
       <div>
         <h1>hello world</h1>
