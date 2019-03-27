b683db7102c6147447ffe40326b94484137817cf
talentamaulana10 <tatamf.id@gmail.com>
2019-03-27 14:47:24 +0700

    import React, { Component } from react;

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import TheScreen from "./react/component/handingEvent/Counter";
+import TheScreen from "./react/component/BindingEventHandlers/Counter";
 
 class App extends Component {
   render() {
