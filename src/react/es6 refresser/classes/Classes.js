deab0efcbe1714bcf82f9b6ada86fd835628cb08
talentamaulana10 <tatamf.id@gmail.com>
2019-03-27 09:11:28 +0700

    learning classe react

--- src/react/es6 refresser/classes/Classes.js
+++ src/react/es6 refresser/classes/Classes.js
@@ -3,14 +3,17 @@ import React, { Component } from 'react'
 export default class Classes extends Component {
   render() {
     class Person {
-        constructor(name){
-          this.name=name;
-        }
-        walk(){
-          console.log("walk")
-        }
+      constructor(name) {
+       this.name = name;
       }
   
+    walk() {
+          console.log("walk");
+      }
+  }
+  
+  const person = new Person('talenta')
+  console.log(person.name);
     return (
       <div>
         <h1>hello world</h1>
--- src/react/es6 refresser/let vs var vs const/script.js
+++
@@ -1,3 +0,0 @@
-const i = 3;
-i = 4;
-console.log(i)
